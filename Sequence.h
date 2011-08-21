6aa00f3c2a37c8f35a7181f34e08be21544b0bf6
Benjamin West <mrgenixus@gmail.com>
2011-08-21 17:09:23 -0600

    Adding Sequence Type

---
+++ Sequence.h
@@ -0,0 +1,16 @@
+#ifndef SKITTLE_SEQUENCE
+#define SKITTLE_SEQUENCE
+
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+class Sequence {
+    
+public:
+    string value;
+    string name;
+};
+
+#endif
